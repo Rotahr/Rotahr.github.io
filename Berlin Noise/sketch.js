a34eb1a859ca6a428d02124209977c093618d096
Dorazahi3903067 <3903067@sps-student.sk.ca>
2021-01-20 13:49:09 -0600

    Terrain Demo / Perlin Noise
    a + d movement through randomly generated terrain using noise() function

--- Berlin Noise/sketch.js
+++ Berlin Noise/sketch.js
@@ -6,11 +6,63 @@
 // - describe what you did to take this project "above and beyond"
 
 
+// let time2 = 0;
+
+let rectHeights;
+let bikeLocation = 0;
+let vroom = 1;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  rectHeights = generateHeight();
+
+  console.log(rectHeights)
+
 }
 
 function draw() {
   background(220);
+
+  let rectNum = width;
+
+  for(let i = bikeLocation; i < bikeLocation + rectNum; i++) {
+    let rectWidth = width / rectNum;
+    rect(rectWidth * (i - bikeLocation), height - rectHeights[i], 
+        rectWidth, rectHeights[i])
+  }
+
+  if (keyIsPressed) {
+    if (key === "d") {
+      bikeLocation += vroom;
+      vroom += 10;
+    }
+    if (key === "a") {
+      bikeLocation -= vroom;
+      vroom -= 10;
+    }
+  }
+
+
+  // let rectHeight = noise(time) * height;
+  // // let y = noise(time2) * height;
+
+  // rect(0, height - rectHeight, 50, rectHeight);
+  // time += 0.005;
+  // // time2 += 0.01;
+
+}
+
+function generateHeight() {
   
+  let theHeights = [];
+  for (let i = 0; i<5000; i++) {
+    let rectHeight = noise(i * 0.001) * height;
+
+    theHeights.push(rectHeight);
+
+  }
+
+return theHeights;
+
 }
