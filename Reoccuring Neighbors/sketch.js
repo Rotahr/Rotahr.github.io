2e2c119e55e5b415f0f5b2714e06f83ab927803c
Rotahr <77638034+Rotahr@users.noreply.github.com>
2021-01-25 14:09:50 -0600

    2d grid project Neighbor thing

---
+++ Reoccuring Neighbors/sketch.js
@@ -0,0 +1,70 @@
+// 2D Array Demo
+// Basic Grid
+
+
+let grid = [[true, true, true],
+            [true, false, true],
+            [true, true, true],
+            [true, false, true]];
+
+let rows;
+let cols;
+let cellWidth;
+let cellHeight;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  //replace Hardcorded grid with empty grid
+
+  rows = grid.length;
+  cols = grid[0].length;
+  cellWidth = width / cols;
+  cellHeight = height / rows;
+  
+
+  noStroke();
+
+}
+
+function draw() {
+  background(220);
+
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === true) {
+        fill("white");
+      }
+      else {
+        fill("black");
+      }
+
+      rect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+    }
+  }
+  // grid[grid.length - 1] = true;
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX / cellWidth);
+  let y = Math.floor(mouseY / cellHeight);
+
+    grid[y][x] = !grid[y][x];
+    grid[y+1][x] = !grid[y+1][x];
+    grid[y][x+1] = !grid[y][x+1];
+    grid[y][x-1] = !grid[y][x-1];
+    grid[y-1][x] = !grid[y-1][x];
+    
+
+}
+
+function createEmptyGrid(cols, rows) {
+  let emptyGrid = [];
+  for (let y = 0; y<rows; y++) {
+    emptyGrid.push([]);
+    for (let x=0; x<cols; x++) {
+      emptyGrid[y].push(true);
+    }
+  }
+  return emptyGrid;
+}
