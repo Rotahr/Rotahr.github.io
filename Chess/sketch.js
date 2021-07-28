cc8c8dc4949a379dd3473d89776de10c2e716b1f
Rotahr <77638034+Rotahr@users.noreply.github.com>
2021-07-28 02:48:17 -0500

    added selecting system as well as a background and foreground grid

--- Chess/sketch.js
+++ Chess/sketch.js
@@ -4,14 +4,16 @@
 
 let pieces = [];
 
+let highlight = "white";
 let rows = 8;
 let cols = 8;
-let grid, cellWidth, cellHeight;
+let grid, cellWidth, cellHeight, backGrid, pnum, my, mx;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+  backGrid = createEmptyGrid(cols, rows);
   grid = createEmptyGrid(cols, rows);
   cellWidth = width / cols;
   cellHeight = height / rows;
@@ -19,23 +21,32 @@ function setup() {
   for (let y=0; y<rows; y++) {
     for (let x=0; x<cols; x++) {
       if (y === 6) {
-        let pawns = new Piece(x, y, "pawn", "white");
+        let pawns = new Piece(x, y, "pawn", "white", "blue");
         pieces.push(pawns);
       }
-      if (y === 1) {
-        let pawns = new Piece(x, y, "pawn", "black");
+      else if (y === 1) {
+        let pawns = new Piece(x, y, "pawn", "black", "red");
         pieces.push(pawns);
       }
+      else {
+        pieces.push(0);
+      }
+    }
+  }
+  for (let i=0; i<pieces.length; i++) {
+    if (pieces[i] !== 0) {
+      pieces[i].display();
     }
   }
 }
 
 class Piece {
-  constructor(x, y, type, side) {
+  constructor(x, y, type, side, fil) {
     this.x = x;
     this.y = y;
     this.type = type;
     this.side = side;
+    this.fil = fil;
   }
 
   display() {
@@ -47,48 +58,67 @@ class Piece {
     }
   }
 
-  Move() {
-    let x = Math.floor(mouseX / cellWidth);
-    let y = Math.floor(mouseY / cellHeight);
-
-    if (grid[y][x] === 9) { 
-      if (grid[y+1][x] === 1) {
-        grid[y][x] = 0;
-      }  
-      else if (grid[y+1][x] === 0) {
-        grid[y][x] = 1;
-      }        
-    }
+  move() {
+    console.log(this.y, this.x);
+    highlight = this.fil;
   }
-}
-
-function mousePressed() {
-  
 }
 
 function draw() {
-  for (let i=0; i<pieces.length; i++) {
-    pieces[i].display();
-  }
+  displayBackGrid();
   displayGrid();
+  strokeWeight(10);
+  fill(highlight);
+  rect(mx * cellWidth, my * cellHeight, cellWidth, cellHeight);
+  strokeWeight(1);
+}
+
+function mousePressed() {
+  mx = Math.floor(mouseX / cellWidth);
+  my = Math.floor(mouseY / cellHeight);
+  pnum = my*8 + mx;
+  console.log(my, mx, grid[my][mx], pnum);
+  pieces[pnum].move();
+  
+  // if (grid[y][x] === 9) { 
+  //   if (grid[y+1][x] === 1) {
+  //     grid[y][x] = 0;
+  //   }  
+  //   else if (grid[y-1][x] === 0) {
+  //     grid[y][x] = 1;
+  //   }        
+  // }
 }
 
 function displayGrid() {
   for (let y=0; y<rows; y++) {
     for (let x=0; x<cols; x++) {
-      if (grid[y][x] === 0) {
-        fill("white");
-        // image(grassImage, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
-      }
-      else if (grid[y][x] === 1) {
-        fill("black");
-        // image(wallImage, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
-      }
-      else if (grid[y][x] === 9) {
+      if (grid[y][x] === 9) {
         fill("red");
+        rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
       }
       else if (grid[y][x] === 8) {
         fill("blue");
+        rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+      }
+      else if (grid[y][x] === 37) {
+        fill("gray");
+        rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+      }
+    }
+  }
+}
+
+function displayBackGrid() {
+  for (let y=0; y<rows; y++) {
+    for (let x=0; x<cols; x++) {
+      if (backGrid[y][x] === 0) {
+        fill("white");
+        // image(grassImage, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+      }
+      else if (backGrid[y][x] === 1) {
+        fill("black");
+        // image(wallImage, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
       }
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
